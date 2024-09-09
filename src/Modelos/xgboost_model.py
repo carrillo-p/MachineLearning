bc758b9e743129b8e74d807b6672d042f7b2fcb7
Alberto <alb.carrillop@gmail.com>
2024-09-09 14:21:13 +0200

    añadidos modelos

--- src/Modelos/xgboost_model.py
+++ src/Modelos/xgboost_model.py
@@ -1,16 +1,73 @@
-import numpy as np
 import pandas as pd
 import xgboost as xgb
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, auc
 import joblib
 
 class XGBoostModel:
     def __init__(self, eval_metric="logloss"):
-        self.model = xgb.XGBClassifier(eval_metric=eval_metric)
+        self.eval_metric = eval_metric
+        self.model = None
+        self.best_hyperparams = None
 
     def prepare_data(self, X, y, test_size=0.3, random_state=42):
         return train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+    def search_hyperparams(self, X_train, X_test, y_train, y_test):
+        space={'max_depth': hp.quniform("max_depth", 3, 18, 1),
+        'gamma': hp.uniform ('gamma', 1,9),
+        'reg_alpha' : hp.quniform('reg_alpha', 40,180,1),
+        'reg_lambda' : hp.uniform('reg_lambda', 0,1),
+        'colsample_bytree' : hp.uniform('colsample_bytree', 0.5,1),
+        'min_child_weight' : hp.quniform('min_child_weight', 0, 10, 1),
+        'n_estimators': 180,
+        'seed': 0
+        }
+        
+        def objective(space):
+            clf = xgb.XGBClassifier(
+                n_estimators=space['n_estimators'],
+                max_depth=int(space['max_depth']),
+                gamma=space['gamma'],
+                reg_alpha=int(space['reg_alpha']),
+                min_child_weight=int(space['min_child_weight']),
+                colsample_bytree=space['colsample_bytree'],  # float entre 0 y 1
+                eval_metric='logloss'  # Desde el constructor
+            )
+            
+            evaluation = [(X_train, y_train), (X_test, y_test)]
+            
+            clf.fit(X_train, y_train,
+                    eval_set=evaluation,
+                    verbose=False)
+            
+            pred = clf.predict(X_test)
+            accuracy = accuracy_score(y_test, pred > 0.5)
+            print("SCORE:", accuracy)
+            return {'loss': -accuracy, 'status': STATUS_OK}
+
+        trials = Trials()
+
+        best_hyperparams = fmin(fn = objective,
+                                space = space,
+                                algo = tpe.suggest,
+                                max_evals = 100,
+                                trials = trials)
+        
+        self.best_hyperparams = best_hyperparams
+        
+    def init_model(self):
+        self.model = xgb.XGBClassifier(
+            n_estimators= 180,
+            max_depth=int(self.best_hyperparams['max_depth']),
+            gamma=self.best_hyperparams['gamma'],
+            reg_alpha=int(self.best_hyperparams['reg_alpha']),
+            reg_lambda=self.best_hyperparams['reg_lambda'],
+            min_child_weight=int(self.best_hyperparams['min_child_weight']),
+            colsample_bytree=self.best_hyperparams['colsample_bytree'],
+            eval_metric=self.eval_metric
+        )
 
     def train(self, X_train, y_train, X_test, y_test):
         eval_set = [(X_train, y_train), (X_test, y_test)]
@@ -48,6 +105,7 @@ class XGBoostModel:
 
     def save_model(self, filename):
         joblib.dump(self.model, filename)
+
 
     @classmethod
     def load_model(cls, filename):
--- train_models.py
+++ train_models.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.Modelos.logistic_model import LogisticModel
 from src.Modelos.xgboost_model import XGBoostModel
+from src.Modelos.stack_model import StackModel
 
 def train_and_save_models():
     # Cargar datos
@@ -17,8 +18,19 @@ def train_and_save_models():
     # Entrenar y guardar modelo XGBoost
     xgboost_model = XGBoostModel()
     X_train, X_test, y_train, y_test = xgboost_model.prepare_data(X, y)
+    xgboost_model.search_hyperparams(X_train, X_test, y_train, y_test) # Buscamos los mejores hiperparametros
+    xgboost_model.init_model()
     xgboost_model.train(X_train, y_train, X_test, y_test)
     xgboost_model.save_model('src/Modelos/xgboost_model.joblib')
+
+
+    #Entrenar y guardar modelo Stack
+    stack_model = StackModel()
+    X_train, X_test, y_train, y_test = logistic_model.prepare_data(X, y)
+    stack_model.search_hyperparams(X_train, X_test, y_train, y_test)
+    stack_model.init_model()
+    stack_model.train(X_train, y_train, X_test, y_test)
+    stack_model.save_model('src/Modelos/stack_model.joblib')
 
 if __name__ == "__main__":
     train_and_save_models()
